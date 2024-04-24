7bd65e29cd198f76b873975e90102a99aae4a0fa
pedrozo1a0 <ale.pedrozo.952@gmail.com>
2024-04-24 09:00:35 -0400

    funcionalidad a todos los botones

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
-function aumentarLikes(){
-    let count=9
-    count++
-    let likesElement= document.querySelector(".likeElement9");
-    likesElement.innerText= count + " like(s)";
+let cantLikes= 9;
+let count=13;
+let lik=9;
+function aumentarLikes(element){
+    let elementSpan= element.previousElementSibling;
+    cantLikes++;
+    elementSpan.textContent= cantLikes +" like(s)";
+
 }
-function aumentarLikes13(){
-    let count=13
-    count++
-    let likesElement= document.querySelector(".likeElement13");
-    likesElement.innerText= count + " like(s)";
+function aumentarLikes13(element){
+    let elementSpan= element.previousElementSibling;
+    count++;
+    elementSpan.textContent= count +" like(s)";
+
 }
-function aumentarLikes92(){
-    let count=9
-    count++
-    let likesElement= document.querySelector(".likeElement92");
-    likesElement.innerText= count + " like(s)";
+
+function aumentarLikes92(element){
+    let elementSpan= element.previousElementSibling;
+    lik++;
+    elementSpan.textContent= lik +" like(s)";
 }
 
