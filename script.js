52bfba3e31a286f8c8f00119b6880c3b4bfbe5e8
pedrozo1a0 <ale.pedrozo.952@gmail.com>
2024-04-23 22:36:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function aumentarLikes(){
+    let count=9
+    count++
+    let likesElement= document.querySelector(".likeElement9");
+    likesElement.innerText= count + " like(s)";
+}
+function aumentarLikes13(){
+    let count=13
+    count++
+    let likesElement= document.querySelector(".likeElement13");
+    likesElement.innerText= count + " like(s)";
+}
+function aumentarLikes92(){
+    let count=9
+    count++
+    let likesElement= document.querySelector(".likeElement92");
+    likesElement.innerText= count + " like(s)";
+}
+
